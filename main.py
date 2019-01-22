2b29bffe5ee7bedb3e6aeb06a350623918d740c1
Matt VerLee <mtverlee@mavs.coloradomesa.edu>
2019-01-22 02:08:29 -0700

    Added sentry.io error tracking.

--- main.py
+++ main.py
@@ -1,30 +1,3 @@
-#pythonNotify
-#By: Matt VerLee
-#mtverlee@mavs.coloradomesa.edu
-#https://github.com/mtverlee/
-
-#MIT License
-
-#Copyright (c) 2017 Matt VerLee
-
-#Permission is hereby granted, free of charge, to any person obtaining a copy
-#of this software and associated documentation files (the "Software"), to deal
-#in the Software without restriction, including without limitation the rights
-#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#copies of the Software, and to permit persons to whom the Software is
-#furnished to do so, subject to the following conditions:
-
-#The above copyright notice and this permission notice shall be included in all
-#copies or substantial portions of the Software.
-
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-#SOFTWARE.
-
 import os
 import requests
 import json
@@ -32,24 +5,27 @@ import httplib
 import urllib
 import smtplib
 from time import strftime
+import sentry_sdk
+sentry_sdk.init("https://ab9b52f468ed4e9fa910d8f2d1bb08f8@sentry.io/1376608")
 
 def sendFBMessage(token, recipient, message):
-    params = {
-        "access_token": token
-    }
-    headers = {
-        "Content-Type": "application/json"
-    }
-    data = json.dumps({
-        "recipient": {
-            "id": recipient
-        },
-        "message": {
-            "text": str(message)
+    try:
+        params = {
+            "access_token": token
         }
-    })
-    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
-    print(r.status_code, r.reason)
+        headers = {
+            "Content-Type": "application/json"
+        }
+        data = json.dumps({
+            "recipient": {
+                "id": recipient
+            },
+            "message": {
+                "text": str(message)
+            }
+        })
+        r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+        print(r.status_code, r.reason)
 
 def sendPushoverNotification(appToken, userToken, message, title, priority):
     conn = httplib.HTTPSConnection("api.pushover.net:443")
