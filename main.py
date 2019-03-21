cead490c5fef8025ae9738bcbe2ad1cad52a13cc
Matt VerLee <mtverlee@mavs.coloradomesa.edu>
2019-03-21 03:17:24 -0600

    Updated sentry.io.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import urllib
 import smtplib
 from time import strftime
 import sentry_sdk
-sentry_sdk.init("https://ab9b52f468ed4e9fa910d8f2d1bb08f8@sentry.io/1376608")
+sentry_sdk.init("https://4faf1b442d25479a87f6789bea08eb9d@sentry.io/1420507")
 
 def sendFBMessage(token, recipient, message):
     try:
@@ -26,52 +26,63 @@ def sendFBMessage(token, recipient, message):
         })
         r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
         print(r.status_code, r.reason)
+    except Exception as e:
+        sentry_sdk.capture_exception(e)
 
 def sendPushoverNotification(appToken, userToken, message, title, priority):
-    conn = httplib.HTTPSConnection("api.pushover.net:443")
-    conn.request("POST", "/1/messages.json",
-    urllib.urlencode({
-        "token": appToken,
-        "user": userToken,
-        "message": message,
-        "title": title,
-        "priority": priority,
-        "retry": 300,
-        "expire": 3600,
-    }), { "Content-type": "application/x-www-form-urlencoded" })
-    print(conn.getresponse())
+    try:
+        conn = httplib.HTTPSConnection("api.pushover.net:443")
+        conn.request("POST", "/1/messages.json",
+        urllib.urlencode({
+            "token": appToken,
+            "user": userToken,
+            "message": message,
+            "title": title,
+            "priority": priority,
+            "retry": 300,
+            "expire": 3600,
+        }), { "Content-type": "application/x-www-form-urlencoded" })
+        print(conn.getresponse())
+    except Exception as e:
+        sentry_sdk.capture_exception(e)
 
 def sendEmail(recipient, subject, text, emailUsername, emailPassword):
-    TIME = strftime("%H:%M:%S")
-    TO = recipient
-    SUBJECT = subject
-    TEXT = text
-    # Gmail Sign In
-    gmail_sender = emailUsername
-    gmail_passwd = emailPassword
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.ehlo()
-    server.starttls()
-    server.login(gmail_sender, gmail_passwd)
-    BODY = '\r\n'.join(['To: %s' % TO,
-                        'From: %s' % gmail_sender,
-                        'Subject: %s' % SUBJECT,
-                        '', TEXT])
     try:
-        server.sendmail(gmail_sender, [TO], BODY)
-        pass
+        TIME = strftime("%H:%M:%S")
+        TO = recipient
+        SUBJECT = subject
+        TEXT = text
+        # Gmail Sign In
+        gmail_sender = emailUsername
+        gmail_passwd = emailPassword
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.ehlo()
+        server.starttls()
+        server.login(gmail_sender, gmail_passwd)
+        BODY = '\r\n'.join(['To: %s' % TO,
+                            'From: %s' % gmail_sender,
+                            'Subject: %s' % SUBJECT,
+                            '', TEXT])
+        try:
+            server.sendmail(gmail_sender, [TO], BODY)
+            pass
+        except Exception as e:
+            print('Encountered exception - ' + e + '.')
+            pass
+        server.quit()
     except Exception as e:
-        print('Encountered exception - ' + e + '.')
-        pass
-    server.quit()
+        sentry_sdk.capture_exception(e)
 
 def sendDiscordWebhook(url, username, avatar_url, content):
-    headers = {
-        "Content-Type" : "application/json"
-    }
-    data = {
-        "username": str(username),
-        "avatar_url": str(avatar_url),
-        "content": str(content)
-    }
-    r = requests.post(str(url), json=data, headers=headers)
+    try:
+        headers = {
+            "Content-Type" : "application/json"
+        }
+        data = {
+            "username": str(username),
+            "avatar_url": str(avatar_url),
+            "content": str(content)
+        }
+        r = requests.post(str(url), json=data, headers=headers)
+    except Exception as e:
+        sentry_sdk.capture_exception(e)
